d252d1025df2e2fd786df341d3d6c3a08825266e
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-02-23 17:23:07 +0000

    cocktail app

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -37,6 +37,6 @@ class DosesController < ApplicationController
   end
 
   def dose_params
-    params.require(:dose).permit(:description, :ingredient_id)
+    params.require(:dose).permit(:description, :ingredient_id, :photo)
   end
 end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,4 +3,5 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
   validates_uniqueness_of :cocktail, {scope: :ingredient}
+  mount_uploader :photo, PhotoUploader
 end
